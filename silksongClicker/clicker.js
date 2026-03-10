ccbb641f524c773ba571bcafa94f26a6262d885b
Delivious <masoncosta31210@gmail.com>
2026-03-10 08:30:20 -0400

    cool stuff

--- silksongClicker/clicker.js
+++ silksongClicker/clicker.js
@@ -1,9 +1,12 @@
 const body=document.querySelector("#body")
 const button=document.querySelector("#black")
 const hornetBtn = document.querySelector("#hornetBtn")
+const hornetPara = document.querySelector("#hornetVal")
 
 let hornetValue = 0
 let multiplier = 1
+let distance = 0
+let distanceX=0
 
 hornetBtn.addEventListener("click",addValue)
 button.addEventListener("click",colorchange)
@@ -18,7 +21,9 @@ function colorchange(){
   
 }
 function addValue(){
+  spawnParticles()
   hornetValue+=multiplier
+  hornetPara = `Rosaries: ${hornetPara}`
   setTimeout(() =>{
       hornetBtn.style.transform="rotate(-4deg) scale(1.16)"
     }, 0)
@@ -79,4 +84,64 @@ function addValue(){
       hornetBtn.style.transform="rotate(0deg) scale(1)"
     }, 140)
   console.log(hornetValue)
+}
+
+function spawnParticles() {
+  const imageRect = hornetBtn.getBoundingClientRect()
+  const centerX = imageRect.left + imageRect.width / 2
+  const centerY = imageRect.top + imageRect.height / 2
+  
+
+  const particleCount = Math.floor(Math.random() * 4) + 5
+  
+  for (let i = 0; i < particleCount; i++) {
+      const particle = document.createElement('img')
+      particle.className = 'particle'
+      particle.src = 'https://www.silk-song.org/_next/image?url=%2Fimages%2Fitems%2Frosaries.png&w=640&q=75'
+    
+      particle.style.left = Math.floor(Math.random() * imageRect.width) + imageRect.left + 'px'
+      particle.style.top = Math.floor(Math.random() * imageRect.height) + imageRect.top + 'px'
+    
+    document.body.appendChild(particle)
+    cashExplode(particle, imageRect)
+    setTimeout(() => {
+      if (particle.parentNode) {
+        particle.parentNode.removeChild(particle)
+      }
+    }, 1000)
+  }
+}
+
+function cashExplode(particle, imageRect) {
+  const startX = parseFloat(particle.style.left)
+  const startY = parseFloat(particle.style.top)
+  const imageRectX = imageRect.left
+  const imageRectY = imageRect.top
+  const imageRectXOpposite = -imageRect.left
+  const imageRectYOpposite = -imageRect.top
+  const duration = 1
+    const angle = (Math.random() - 0.5) * Math.PI / 2 
+    const speed = Math.random() * 200 + 150              
+    let vx = Math.cos(angle) * speed
+    let vy = -Math.abs(Math.sin(angle) * speed)
+
+  const gravity = 600 
+  const steps = 50
+
+  for (let i = 0; i <= steps; i++) {
+    setTimeout(() => {
+      const t = (i / steps) * duration
+      const x = startX + vx * t
+      const y = startY + vy * t + 0.5 * gravity * t * t
+      
+      if (x < imageRectX || x > imageRectXOpposite) {
+        vx -= 50 * t
+      } 
+      else {
+        vx += 50 * t 
+      }
+      particle.style.left = x + 'px'
+      particle.style.top = y + 'px'
+    }, (i * duration / steps) * 1000)
+  }
 }
